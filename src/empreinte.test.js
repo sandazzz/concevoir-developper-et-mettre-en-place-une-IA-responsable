a133ba5658dd6b304383906f4afbf9a18ec3490f
Sandarisoa Rakotovelo <sandarisoa.rakotovelo@francetravail.fr>
2026-02-22 19:53:59 +0100

    feat: green - ajout des tests unitaires pour calculEmpreinteCPU

--- src/empreinte.test.js
+++ src/empreinte.test.js
@@ -8,6 +8,18 @@ describe("calculEmpreinteCPU", () => {
   test("Initialisation", () => {
     expect(calculEmpreinteCPU).toBeDefined();
   });
+
+  test("1 CPU", () => {
+    expect(calculEmpreinteCPU(1)).toBe(21);
+  });
+
+  test("2 CPU", () => {
+    expect(calculEmpreinteCPU(2)).toBe(42.1);
+  });
+
+  test("6 CPU", () => {
+    expect(calculEmpreinteCPU(6)).toBe(126.2);
+  });
 });
 
 describe("calculEmpreinteRAM", () => {
