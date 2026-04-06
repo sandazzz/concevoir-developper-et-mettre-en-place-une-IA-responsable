7549808a44b891a44630511de9b46c3e53f036a5
Sandarisoa Rakotovelo <rsandarisoa@gmail.com>
2026-04-06 05:02:19 +0200

    feat: add closeModal functionality and clean up modal button event handling in index.html

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import ScrollAnimations from "./src/features/scroll-animations.js";
 import DecisionTree from "./src/features/decision-tree.js";
 import AnalyseResponse from "./src/features/analyse-response.js";
 import { createCalculator } from "./src/features/footprint/calculator.js";
+import { closeModal } from "./src/features/close-modal.js";
 
 const App = (() => {
   const init = () => {
@@ -12,6 +13,7 @@ const App = (() => {
     ScrollAnimations();
     AnalyseResponse();
     DecisionTree();
+    closeModal();
     console.log("Formation IA Responsable - Site initialisé");
   };
 
